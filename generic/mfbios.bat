33dca8f8ec66b4019f53e07a64624ff867ca89cb
Paul Edwards <mutazilah@gmail.com>
2025-07-11 12:24:29 +1000

    add doco

--- generic/mfbios.bat
+++ generic/mfbios.bat
@@ -1,5 +1,11 @@
 rem build z/pdos-generic pseudo-bios
+rem suitable for running ZPG directly under mfemul or Hercules/380
+rem as opposed to using z/OS PCDOS with mvspb.bat
+
 rem You need to have run pdpzpb.bat in pdpclib first
+
+rem This can then be combined with makefile.zpg and makecomm.zpg
+
 
 gccmvs -Os -U__MVS__ -DNEED_MVS -D__MF32__ -D__ZPDOSGPB__ -DUSE_MEMMGR -S -I . -I ../pdpclib -I ../bios -I ../src bios.c
 gccmvs -Os -U__MVS__ -DNEED_MVS -D__MF32__ -D__ZPDOSGPB__ -DUSE_MEMMGR -S -I . -I ../pdpclib -I ../bios -I ../src ../bios/exeload.c
